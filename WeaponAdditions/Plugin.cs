6d9f2cad31fa37b6e5edd561b61e84f444f169ce
blacks7ar <129362030+blacks7ar-dev@users.noreply.github.com>
2025-03-20 20:59:27 +0800

    updated serversync

--- WeaponAdditions/Plugin.cs
+++ WeaponAdditions/Plugin.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
@@ -17,7 +17,7 @@ namespace WeaponAdditions
         private const string modGUID = "blacks7ar.WeaponAdditions";
         public const string modName = "WeaponAdditions";
         public const string modAuthor = "blacks7ar";
-        public const string modVersion = "1.2.2";
+        public const string modVersion = "1.2.3";
         public const string modLink = "https://valheim.thunderstore.io/package/blacks7ar/WeaponAdditions/";
         private static readonly Harmony _harmony = new(modGUID);
 
